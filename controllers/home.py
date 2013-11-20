5fe787b5249dd239fd4a483b151e9a069ffbb910
Osei-Bonsu Christian <christian.kacob@gmail.com>
2013-11-20 11:53:03 +0000

    finished resolving both get and post rendering of search page

--- controllers/home.py
+++ controllers/home.py
@@ -1,29 +1,7 @@
 
 from security import Root
 from models import Tourist
-from models import Guide
-from google.appengine.ext import db
-from google.appengine.api import urlfetch
-import urllib
-import json
 
-
-def getCountryFromJson(jsonResponse):
-    reponseResults = jsonResponse["results"]
-    components_list = reponseResults[0]["address_components"]
-    # return components_list[0]["types"][0][0]
-    country = ""
-
-    count = 0
-    for component in components_list:
-        component_type = components_list[count]["types"]
-        
-        if component_type[0] == "country":
-            country = component["long_name"]
-
-        count += 1
-
-    return country
 
 class Home(Root.Handler):
     def get(self):
@@ -39,21 +17,10 @@ class Home(Root.Handler):
     	departure_date = self.request.get("departure")
 
     	if destination and arrival_date and departure_date:
-            search_args = {"destination" : destination, "arrival_date" : arrival_date, "departure_date" : departure_date}
-
-            request = urlfetch.Fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" 
-                + urllib.quote(search_args["destination"].encode("utf-8")) + "&sensor=true").content
-
-            request_json = json.loads(request)
-            destination_country = getCountryFromJson(request_json)
-
-            suggested_guides = db.GqlQuery("select * from Guide")
-
-            guide = Guide.Guide(_country = "Ghana", _picture = "https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yo/r/UlIqmHJn-SK.gif",
-                _firstname = "David", _lastname = "Thomson", _email = "david@gmail.com")
-            guide.put()
+            # guide = Guide.Guide(_country = "Ghana", _picture = "https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yo/r/UlIqmHJn-SK.gif",
+            #     _firstname = "David", _lastname = "Thomson", _email = "david@gmail.com")
+            # guide.put()
             # self.render("search.html", suggested_guides = suggested_guides, search_args = search_args)
-            self.redirect("/search?destination=" + destination_country + "&arrival_date=" + arrival_date + 
-                "&departure_date=" + departure_date)
+            self.redirect("/search?destination=" + destination + "&arrival_date=" + arrival_date + "&departure_date=" + departure_date)
     	else:
     		self.render("index.html", error_message = "Please provide all details to complete search")
--- controllers/search.py
+++ controllers/search.py
@@ -1,13 +1,45 @@
 from security import Root
 from models import Guide
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
+import urllib
+import json
 
+def getCountryFromJson(jsonResponse):
+    reponseResults = jsonResponse["results"]
+    components_list = reponseResults[0]["address_components"]
+    # return components_list[0]["types"][0][0]
+    country = ""
+
+    count = 0
+    for component in components_list:
+        component_type = components_list[count]["types"]
+        
+        if component_type[0] == "country":
+            country = component["long_name"]
+
+        count += 1
+
+    return country
 
 class Search(Root.Handler):
     def get(self):
-    	suggested_guides = Guide.Guide.all()
     	destination = self.request.get("destination")
     	arrival_date = self.request.get("arrival_date")
     	departure_date = self.request.get("departure_date")
-    	
+
     	search_args = {"destination" : destination, "arrival_date" : arrival_date, "departure_date" : departure_date}
+
+    	suggested_guides = None
+    	if destination:
+    		request = urlfetch.Fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" 
+                + urllib.quote(search_args["destination"].encode("utf-8")) + "&sensor=true").content
+
+    		request_json = json.loads(request)
+    		destination_country = getCountryFromJson(request_json)
+
+    		suggested_guides = Guide.Guide.gql("where _country = :1", destination_country)
+
         self.render("search.html", suggested_guides = suggested_guides, search_args = search_args)
+        
+        	
