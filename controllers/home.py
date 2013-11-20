ead43c8471ea43d59fdf5a85347724246fba0758
Osei-Bonsu Christian <christian.kacob@gmail.com>
2013-11-20 06:22:33 +0000

    Completed geocoding to extract country from address and match with guide's country

--- controllers/home.py
+++ controllers/home.py
@@ -3,7 +3,27 @@ from security import Root
 from models import Tourist
 from models import Guide
 from google.appengine.ext import db
+from google.appengine.api import urlfetch
+import urllib
+import json
 
+
+def getCountryFromJson(jsonResponse):
+    reponseResults = jsonResponse["results"]
+    components_list = reponseResults[0]["address_components"]
+    # return components_list[0]["types"][0][0]
+    country = ""
+
+    count = 0
+    for component in components_list:
+        component_type = components_list[count]["types"]
+        
+        if component_type[0] == "country":
+            country = component["long_name"]
+
+        count += 1
+
+    return country
 
 class Home(Root.Handler):
     def get(self):
@@ -19,8 +39,16 @@ class Home(Root.Handler):
     	departure_date = self.request.get("departure")
 
     	if destination and arrival_date and departure_date:
-    		search_args = {"destination" : destination, "arrival_date" : arrival_date, "departure_date" : departure_date}
-    		suggested_guides = db.GqlQuery("select * from Guide")
-    		self.render("search.html", suggested_guides = suggested_guides, search_args = search_args)
+            search_args = {"destination" : destination, "arrival_date" : arrival_date, "departure_date" : departure_date}
+
+            request = urlfetch.Fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" 
+                + urllib.quote(search_args["destination"].encode("utf-8")) + "&sensor=true").content
+
+            request_json = json.loads(request)
+            destination_country = getCountryFromJson(request_json)
+
+            suggested_guides = db.GqlQuery("select * from Guide where country = :1", destination_country)
+
+            self.render("search.html", suggested_guides = suggested_guides, search_args = search_args)
     	else:
     		self.render("index.html", error_message = "Please provide all details to complete search")
