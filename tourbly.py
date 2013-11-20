ee790b703e6a62a34ce29305d701fdc9ee85cc5b
Nana Ewusi <precious.ewusi.nyarko@meltwater.org>
2013-11-20 00:29:48 +0000

    Added route to fetch specific guides page

--- tourbly.py
+++ tourbly.py
@@ -21,7 +21,7 @@ from controllers import places
 from controllers import profile
 from controllers import verifyemail
 from controllers import search
-
+from controllers import guide
 
 
 # APPLICATION ENTRY
@@ -34,16 +34,17 @@ class tourbly(Root.Handler):
 # URI ROUTING
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/',               handler=tourbly,                    name='root'),
-    webapp2.Route(r'/home',           handler=home.Home,                  name='home'),
-    webapp2.Route(r'/img',            handler=image.Image,                name='images'),
-    webapp2.Route(r'/signin',         handler=signin.Signin,              name='signin'),
-    webapp2.Route(r'/signup',         handler=signup.Signup,              name='signup'),
-    webapp2.Route(r'/logout',         handler=logout.Logout,              name='logout'),
-    webapp2.Route(r'/places',         handler=places.Places,              name='places'),
-    webapp2.Route(r'/profile',        handler=profile.Profile,            name='profile'),
-    webapp2.Route(r'/verify_email',   handler=verifyemail.VerifyEmail,    name='verify'),
-    webapp2.Route(r'/search',         handler=search.Search,              name='search')
+    webapp2.Route(r'/',                     handler=tourbly,                    name='root'),
+    webapp2.Route(r'/home',                 handler=home.Home,                  name='home'),
+    webapp2.Route(r'/img',                  handler=image.Image,                name='images'),
+    webapp2.Route(r'/signin',               handler=signin.Signin,              name='signin'),
+    webapp2.Route(r'/signup',               handler=signup.Signup,              name='signup'),
+    webapp2.Route(r'/logout',               handler=logout.Logout,              name='logout'),
+    webapp2.Route(r'/places',               handler=places.Places,              name='places'),
+    webapp2.Route(r'/profile',              handler=profile.Profile,            name='profile'),
+    webapp2.Route(r'/verify_email',         handler=verifyemail.VerifyEmail,    name='verify'),
+    webapp2.Route(r'/search',               handler=search.Search,              name='search'),
+    webapp2.Route(r'/guides/ezu',     handler=guide.Guide,                name='guide')
 
 ], debug=True)          # CHANGE TO False BEFORE FINAL DEPLOYMENT
 
