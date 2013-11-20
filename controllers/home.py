2cfd75890e8e6a02b1c40e885a640a39afab2723
Osei-Bonsu Christian <christian.kacob@gmail.com>
2013-11-20 01:09:21 +0000

    working on search for guides

--- controllers/home.py
+++ controllers/home.py
@@ -10,3 +10,16 @@ class Home(Root.Handler):
             self.render("index.html", isLoggedIn = self.check_session("query"), tourist = tourist)
         else:
             self.render("index.html", isLoggedIn = self.check_session("query"))
+
+    def post(self):
+    	destination = self.request.get("destination")
+    	arrival_date = self.request.get("arrival")
+    	departure_date = self.request.get("departure")
+
+    	if destination and arrival_date and departure_date:
+    		# self.render("search.html")
+    		self.write(destination + " " + arrival_date + " " + departure_date)
+    		suggested_guides = Guide.qql()
+    		self.render("search.html", suggested_guides = suggested_guides)
+    	else:
+    		self.render("index.html", error_message = "Please provide all details to complete search")
