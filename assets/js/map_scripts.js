714c3380f63c8c7efe1880d3b00f515840b48d80
Osei-Bonsu Christian <christian.kacob@gmail.com>
2013-11-25 10:08:43 +0000

    Restricted autocomplete options to africa

--- assets/js/map_scripts.js
+++ assets/js/map_scripts.js
@@ -3,7 +3,7 @@ function init() {
 	
     var destination_tag = document.getElementById('destination');
 	var options = {
-	componentRestrictions: {country: 'gh'}
+	componentRestrictions: {country: 'au'}
 	};
 	var place;
 	var autocomplete = new google.maps.places.Autocomplete(destination_tag, options);
