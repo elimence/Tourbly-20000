65028cd1b00ab9be8359fbcd0a7a9bf2137fc79f
nanaewusi <precious.ewusi.nyarko@meltwater.org>
2014-01-02 15:19:35 +0000

    Added functionality to prevent blank field submissions when writing a new review

--- assets/js/guide.js
+++ assets/js/guide.js
@@ -4,4 +4,29 @@ function init() {
 		// and swap the text out with the timeago equivalent
 		$(item).html(moment($(item).html(), "YYYY-MM-DD HH:mm:ss").fromNow());
 	});
+  attachSubmitReviewListener();
+}
+
+function attachSubmitReviewListener() {
+  if ($('#submit-btn').length) {
+    $('#submit-btn').on('click', submitReviewClickListener);
+  }
+}
+
+function submitReviewClickListener(e) {
+  var nameInputPresent = $('#name').length;
+  var rating = $('#rating').val(),
+      comment = $('#comment').val();
+  if (nameInputPresent) {
+    var name   = $('#name').val();
+    if (!(name && rating && comment)) {
+      e.preventDefault();
+      alert('All fields are required to save your review');
+    }
+  } else {
+    if (!(rating && comment)) {
+      e.preventDefault();
+      alert('All fields are required to save your review');
+    }
+  }
 }
