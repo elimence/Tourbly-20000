c6417e306bf37b2eb51c63c888e0113c98299f4e
Osei-Bonsu Christian <christian.kacob@gmail.com>
2014-01-02 23:42:03 +0000

    work in progress on profile page

--- controllers/image.py
+++ controllers/image.py
@@ -10,13 +10,7 @@ class Image(Root.Handler):
         entity_id = int(self.request.get('entity_id'))
         model = self.request.get("model")
 
-        if model == "Guide":
-        	entity = Guide.Guide.get_by_id(entity_id)
-        elif model == "Tourist":
-        	entity = Tourist.Tourist.get_by_id(entity_id)
-        elif model == "Place":
-            entity = Destination.Destination.get_by_id(entity_id)
+        entity = Tourist.Tourist.get_by_id(entity_id)
         if entity.picture:
-        	# picture = urlfetch.Fetch(entity.picture).content
         	self.response.headers['Content-Type'] = 'image/png'
         	self.write(entity.picture)
